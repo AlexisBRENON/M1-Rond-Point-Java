01bbe9ec3fca4ecab27813bd8a7e9db93a2bfbff
cecilia57 <cecilia57@97df086e-520d-456a-832f-0cef65719be7>
2012-11-28 14:53:43 +0000

    ajout Enum des voies création de la méthode ajout de voiture dans simulateur

--- trunk/rondpoint_gl/src/aclt/genielog/rp/Simulateur.java
+++ trunk/rondpoint_gl/src/aclt/genielog/rp/Simulateur.java
@@ -1,6 +1,7 @@
 package aclt.genielog.rp;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -8,7 +9,7 @@ import java.util.concurrent.TimeUnit;
  * @author tiph
  */
 public class Simulateur extends Thread {
-	
+
 	private final RondPoint rp;
 
 	public Simulateur(int taille) {
@@ -28,12 +29,15 @@ public class Simulateur extends Thread {
 	/**
 	 * Mets en pause pendant un temps donné.
 	 * 
-	 * @param duration Le nombre d'unité de temps à attendre.
-	 * @param unit L'untité de temps.
+	 * @param duration
+	 *            Le nombre d'unité de temps à attendre.
+	 * @param unit
+	 *            L'untité de temps.
 	 */
 	private void pause(long duration, TimeUnit unit) {
 		long pause;
-		pause = TimeUnit.NANOSECONDS.toMillis(System.nanoTime()) + unit.toMillis(duration);
+		pause = TimeUnit.NANOSECONDS.toMillis(System.nanoTime())
+				+ unit.toMillis(duration);
 		while (TimeUnit.NANOSECONDS.toMillis(System.nanoTime()) < pause) {
 			try {
 				sleep(pause - TimeUnit.NANOSECONDS.toMillis(System.nanoTime()));
@@ -43,14 +47,77 @@ public class Simulateur extends Thread {
 		}
 	}
 
+	public VoieEnum voieAleatoire() {
+		Random r = new Random();
+		VoieEnum v=VoieEnum.NORD;
+		switch (r.nextInt(4)) {
+		case 0:
+			v = VoieEnum.NORD;
+			break;
+		case 1:
+			v = VoieEnum.EST;
+			break;
+		case 2:
+			v = VoieEnum.SUD;
+			break;
+		case 3:
+			v = VoieEnum.OUEST;
+			break;
+		}
+		return v;
+		
+
+	}
+
+	public void ajout(int n, VoieEnum entree, VoieEnum sortie) {
+		int entreeInt, sortieInt, i;
+
+		if(entree == VoieEnum.ALEAT && sortie == VoieEnum.ALEAT){
+			for(i=0; i<n ; i++){
+				entree = voieAleatoire();
+				sortie = voieAleatoire();
+				entreeInt = entree.ordinal();
+				sortieInt = sortie.ordinal();
+				Voiture.factory(rp, entreeInt, sortieInt);
+			}
+		}
+		
+		else if (entree == VoieEnum.ALEAT) {
+			sortieInt = sortie.ordinal();
+			for(i=0; i<n ; i++){
+				entree = voieAleatoire();
+				entreeInt = entree.ordinal();
+				Voiture.factory(rp, entreeInt, sortieInt);
+			}		
+		}
+
+		else if (sortie == VoieEnum.ALEAT) {
+			entreeInt = entree.ordinal();
+			for(i=0; i<n ; i++){
+				sortie = voieAleatoire();
+				sortieInt = sortie.ordinal();
+				Voiture.factory(rp, entreeInt, sortieInt);
+			}
+		}
+		
+		else{
+			entreeInt = entree.ordinal();
+			sortieInt = sortie.ordinal();
+			for (i = 0; i < n; i++) {
+				Voiture.factory(rp, entreeInt, sortieInt);
+			}
+		}
+	}
+
 	public static void main(String argv[]) {
 
 		Simulateur sim = new Simulateur(2);
-		ArrayList<Voiture> voitures = new ArrayList<Voiture>();
-		for (int i = 0; i < 3; i = i + 1) {
-			voitures.add(Voiture.factory(sim.rp, 0, 0));
-		}
+		//ArrayList<Voiture> voitures = new ArrayList<Voiture>();
+		// for (int i = 0; i < 3; i = i + 1) {
+		// voitures.add(Voiture.factory(sim.rp, 0, 0));
+		// }
 
 		sim.start();
+		sim.ajout(5, VoieEnum.ALEAT, VoieEnum.ALEAT);
 	}
 }
